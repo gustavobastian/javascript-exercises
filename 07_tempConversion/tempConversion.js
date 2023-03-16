6519de45d04a29baa3a177a74b1f5123486ac393
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-03-16 17:21:16 -0300

    first  test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let out=temperature;
+
+  if(temperature==32){
+    out=0;
+  }
+
+  return out;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let out=temperature
+  return out;
 };
 
 // Do not edit below this line
