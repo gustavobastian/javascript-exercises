1cd8b2929f97846d62fa57d18d39e096ed9b06de
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-03-16 16:10:28 -0300

    repeatString first commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(data,numberP) {
+    let number=parseInt(numberP);
+    let out="";
+    
 
+    for (let i=0;i<number;i++)
+    {        
+        let local=out;
+        out=local+data;
+    }
+    return out;
 };
 
 // Do not edit below this line
