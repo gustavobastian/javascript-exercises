3185c04729ba8dc608057a4392a9a43fd8291c96
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-03-16 17:30:16 -0300

    all test passing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(temperature) {
   let out=(Math.round((temperature-32)*50/9))/10;  
-
   return out;
 };
 
 const convertToFahrenheit = function(temperature) {
-  let out=temperature
+  
+  let out=(Math.round(((temperature*9/5)+32)*10))/10;  
   return out;
 };
 
