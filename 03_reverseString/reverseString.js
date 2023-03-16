e3c5a514a13f3470d27f4890d9b084b36c01632b
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-03-16 16:38:53 -0300

    exercise 3, first passed test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(data) {
+    let info="";
+    
+    for (let i = data.length-1;i>=0;i--){
+        info=info.concat(data[i]);
+    }
+    
+    return info;
 };
 
 // Do not edit below this line
