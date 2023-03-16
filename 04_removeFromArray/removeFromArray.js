594717afee9c0e6fecb1881ab1bb49f13ee403e7
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-03-16 16:49:30 -0300

    exercise 4 fist test pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(data,argument) {
 
+    let index=data.indexOf(argument);
+    let out= [];
+    for (let i=0;i<data.length;i++){
+        if(i!= index){
+            out.push(data[i]);
+        }
+    }
+
+    return out;
 };
 
 // Do not edit below this line
