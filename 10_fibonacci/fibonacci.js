8c385392250f165307ea680511f457d833e4e001
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-03-17 19:40:40 -0300

    fibonacci first two test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number==0){
+        return 0;
+    }
+    if(number==1 || (number == 2)){
+        return 1;
+    }
+    else{
+        return fibonacci(number-1) + fibonacci(number-2)
+    }
 
 };
 
