ff77db1b7595249fc00154ab45b166964ec08fbd
Gustavo Bastian <bastian.gustavo@gmail.com>
2023-03-17 19:36:14 -0300

    pass all test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (arrayp) {
+    let array=[];
+    for (const element of arrayp){
+        if ((element=='!')||(element=='.')||(element==',')||(element==' '))
+        {
+          continue;
+        }
+        else{
+            array.push(element.toLowerCase());
+        }
+    }        
+    let value=(array.length)/2;
+    for (let i= 0;i<value;i++){  
+        
+        if(array[i]!=array[array.length-i-1])
+        {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
